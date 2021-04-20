ba18e805c8b996bfb8604b1e922f4c1e9117cabf
tranba <batt@reactplus.com>
2021-04-20 23:05:30 +0700

    fix sever

--- src/config/api/server.js
+++ src/config/api/server.js
@@ -5,6 +5,7 @@ const port = 4000
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./db.js')
+const productRoutes = require('../model/product.route.js')
 
 mongoose.Promise = global.Promise
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -14,6 +15,8 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.use('/products', productRoutes)
 
 app.listen(port, () => {
     console.log('Server is running on http://localhost:', port)
